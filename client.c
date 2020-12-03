d1a845e3ddf5caea63439e46661be32857eb7906
Duowen <15011128926@163.com>
2020-12-03 14:36:31 +0800

    network connectivity test program

---
+++ client.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+int main(){
+    int sockfd;
+    char buffer[1024];
+    struct sockaddr_in server={0};
+    int port = 5050 ,nbytes;
+    char *serverip = "192.168.31.10";
+
+    if((sockfd = socket(AF_INET , SOCK_STREAM , 0))==-1){
+        perror("Socket fail : ");
+        exit(1);
+    }
+    server.sin_family = AF_INET;
+
+    if(inet_aton(serverip , &server.sin_addr)==0){
+        printf("The server IP is not right !\n");
+        exit(1);
+    }
+    server.sin_port = htons(port);
+    if((connect(sockfd , (struct sockaddr *)(&server) , sizeof(struct sockaddr)))==-1){
+        perror("Connect fail : ");
+        exit(1);
+    }
+    nbytes = read(sockfd , buffer , 1024);
+    if(nbytes < 0){
+        printf("Read error:%s\n",strerror(errno));
+        exit(1);
+    }
+    close(sockfd);
+    printf("Received %d bytes :%s\n",nbytes , buffer);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(){
+    int sockfd,new_fd;
+    int sin_size;
+    int port=5050;
+    struct sockaddr_in server={0};
+    struct sockaddr_in client={0};
+
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+    if(sockfd==-1){
+        perror("Socket error");
+        exit(1);
+    }
+
+
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=htonl(INADDR_ANY);
+    server.sin_port=htons(port);
+
+    if(-1==bind(sockfd,(struct sockaddr *)(&server),sizeof(struct sockaddr))){
+        perror("Bind error");
+        exit(1);
+    }
+
+    if(-1==listen(sockfd,1)){
+        perror("Listen error");
+        exit(1);
+    }
+
+    
+    new_fd=accept(sockfd,(struct sockaddr*)(&client),&sin_size);
+    if(new_fd==-1){
+        perror("Accept error");
+        exit(1);
+    }
+
+    if(write(new_fd,"hello from the other side",strlen("hello from the other side"))==-1){
+        perror("Write error");
+        exit(-1);
+    }
+
+    close(new_fd);
+    close(sockfd);
+
+    return 0;
+}
